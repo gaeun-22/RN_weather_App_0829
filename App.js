6a6809b4fff5af72b064b94203b70f6141155fc9
gaeun-22 <gaeunnam31@gmail.com>
2025-09-02 18:11:26 +0900

    스타일 적용 및 레이아웃

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}> 결과 : {number} </Text>
+      {/*전개구문*/}
+      <Text style={{...styles.text, color: "blue"}}> 리액트 네이티브</Text> 
+      <View style={{ flexDirection: "row", gap: 10}}>
+        <Button title="증가" onPress={() => setNumber(number +1)}/>
+        <Button title="감소" onPress={() => setNumber(number -1)}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +23,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
+  text: { //부모속성 상속x
+    fontSize: 50,
+    fontWeight: "bold",
+  }
 });
